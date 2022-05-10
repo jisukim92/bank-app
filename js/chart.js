b7a8abf02435c2895a7792cf84ded3ddad22bb40
jisukim92 <kjs2504@gmail.com>
2022-05-10 15:13:42 +0900

    feat: Modified report page

--- js/chart.js
+++ js/chart.js
@@ -1,15 +1,14 @@
-fetch('bank.json')
+fetch('data.json')
   .then(function (res) {
     return res.json()
   })
   .then(function (obj) {
-    setGraph(obj)
+    setLineGraph(obj)
+    setDoughnutGraph(obj)
   })
 
-function setGraph(obj) {
+function setLineGraph(obj) {
   const bankList = obj['bankList'] //.filter((e, i) => i < 10)
-  console.log(bankList[0])
-  console.log(new Date(bankList[0]['date']).getMonth() + 1)
 
   const groupBy = function (data, key) {
     return data.reduce(function (carry, el) {
@@ -24,20 +23,20 @@ function setGraph(obj) {
     }, {})
   }
 
-  let groupByBankList = groupBy(bankList, 'date')
+  let groupByBankList = groupBy(obj, 'date') //날짜 별 그룹
   console.log('groupByBankList')
   console.log(groupByBankList)
 
   let labels = []
   let totalOut = []
   for (const [key, value] of Object.entries(groupByBankList)) {
-    console.log(key)
-    console.log(new Date(key).getDate())
+    // console.log(key)
+    // console.log(new Date(key).getDate())
     labels.push(('00' + new Date(key).getDate()).slice(-2))
     totalOut.push(totalAmount(value))
   }
-  console.log(labels)
-  console.log(totalOut)
+  // console.log(labels)
+  // console.log(totalOut)
 
   const data = {
     labels: labels,
@@ -45,25 +44,17 @@ function setGraph(obj) {
       {
         label: '일간 리포트',
         data: totalOut,
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-          'rgba(255, 205, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(201, 203, 207, 0.2)',
-        ],
-        borderColor: [
-          'rgb(255, 99, 132)',
-          'rgb(255, 159, 64)',
-          'rgb(255, 205, 86)',
-          'rgb(75, 192, 192)',
-          'rgb(54, 162, 235)',
-          'rgb(153, 102, 255)',
-          'rgb(201, 203, 207)',
-        ],
+        backgroundColor: ['rgba(75, 192, 192, 0.2)'],
+        borderColor: ['rgb(75, 192, 192)'],
         borderWidth: 1,
+      },
+      {
+        label: '선 그래프',
+        data: totalOut,
+        borderColor: 'rgb(54, 162, 235)',
+        backgroundColor: 'rgb(54, 162, 235)',
+        type: 'line',
+        order: 0,
       },
     ],
   }
@@ -80,15 +71,129 @@ function setGraph(obj) {
     },
   }
 
-  const myChart = new Chart(document.getElementById('myChart'), config)
+  const lineBarChart = new Chart(
+    document.getElementById('lineBarChart'),
+    config
+  )
+}
+
+function setDoughnutGraph(obj) {
+  const currentMonth = new Date().getMonth() + 1
+  const patternTitle = document.querySelector('.pattern_title')
+  patternTitle.textContent = `${currentMonth}월 지출 패턴`
+  const bankList = obj.filter((e, i) => {
+    if (
+      new Date(e['date']).getMonth() + 1 === new Date().getMonth() + 1 &&
+      e['inOut'] === 'out'
+    ) {
+      return e
+    }
+  })
+  console.log(bankList)
+  // console.log(new Date(obj[0]['date']).getMonth() + 1)
+
+  const groupBy = function (data, key) {
+    return data.reduce(function (carry, el) {
+      let group = el[key] || 'others'
+      // console.log('group')
+      // console.log(group)
+
+      if (carry[group] === undefined) {
+        carry[group] = []
+      }
+
+      carry[group].push(el)
+      return carry
+    }, {})
+  }
+
+  let groupByBankList = groupBy(bankList, 'type') //타입 별 그룹
+  console.log('groupByBankList')
+  console.log(groupByBankList)
+
+  let doughnutLabels = []
+  let totalOut = []
+  for (const [key, value] of Object.entries(groupByBankList)) {
+    console.log(key)
+    // console.log(new Date(key).getDate())
+    doughnutLabels.push(key)
+    totalOut.push(totalAmount(value))
+  }
+  console.log(doughnutLabels)
+  console.log(totalOut)
+
+  const data = {
+    labels: doughnutLabels,
+    datasets: [
+      {
+        label: 'My First Dataset',
+        data: totalOut,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.8)',
+          'rgba(54, 162, 235, 0.8)',
+          'rgba(153, 102, 255, 0.8)',
+          'rgba(201, 203, 207, 0.8)',
+        ],
+        hoverOffset: 4,
+      },
+    ],
+  }
+
+  const config = {
+    type: 'doughnut',
+    data: data,
+  }
+
+  const doughnutChart = new Chart(
+    document.getElementById('doughnutChart'),
+    config
+  )
+
+  const typeListEl = document.querySelector('.type_list > ul')
+  doughnutLabels.map((e, i) => {
+    const liEl = document.createElement('li')
+    const divTypeInfo = document.createElement('div')
+    divTypeInfo.classList.add('type_info')
+    const divEl1 = document.createElement('div')
+    // const imgEl = document.createElement('img')
+    divEl1.classList.add('type_img')
+    if (e === 'eatout') {
+      var img = new Image()
+      img.src = '../img/eatout.svg'
+      divEl1.appendChild(img)
+    } else if (e === 'transport') {
+      var img = new Image()
+      img.src = '../img/transport.svg'
+      divEl1.appendChild(img)
+    } else if (e === 'shopping') {
+      var img = new Image()
+      img.src = '../img/shopping.svg'
+      divEl1.appendChild(img)
+    }
+
+    const divEl2 = document.createElement('div')
+    divEl2.classList.add('type_text')
+    divEl2.textContent = e
+
+    const divEl3 = document.createElement('div')
+    divEl3.classList.add('type_amount')
+    divEl3.textContent = Number(totalOut[i]).toLocaleString() + '원'
+
+    divTypeInfo.appendChild(divEl1)
+    divTypeInfo.appendChild(divEl2)
+    liEl.appendChild(divTypeInfo)
+    liEl.appendChild(divEl3)
+    typeListEl.appendChild(liEl)
+  })
 }
 
 function getdate(val) {}
 //총 지출 계산 함수
 function totalAmount(value) {
+  console.log(value)
   var ans = 0
   value.map((ele) => {
-    if (ele['income'] === 'out') {
+    if (ele['inOut'] === 'out') {
       ans += ele['price']
     }
   })
--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-fetch('https://eulsoo.github.io/list.json')
+fetch('data.json')
   .then(function (res) {
     return res.json()
   })
@@ -20,7 +20,10 @@ function 할일(obj) {
     }, {})
   }
 
-  let historyList = groupBy(obj.reverse(), 'date')
+  // console.log(obj)
+  let historyList = groupBy(obj, 'date')
+  console.log('historyList')
+  console.log(historyList)
 
   const historyEl = document.querySelector('.history-scroll')
   const historyDayEl = document.querySelector('.history-day')
@@ -102,3 +105,112 @@ function 할일(obj) {
     return ulElem
   }
 }
+
+//////////////////////////////////////////
+//drag Event
+
+// let drageable = false
+// let currentY = 0
+const dragBtn = document.querySelector('.drag-btn')
+const historyScroll = document.querySelector('.history-scroll')
+let clicked = false
+// const initYEl = document.querySelector('.initY')
+// const firstYEl = document.querySelector('.firstY')
+
+let historywrap = document.getElementById('history-wrap'),
+  //   // initX,
+  initY,
+  //   // firstX,
+  firstY
+
+dragBtn.addEventListener('mousedown', function (e) {
+  // console.log(e.currentY)
+  clicked = !clicked
+  if (clicked) {
+    historywrap.style.top = '90px'
+    historyScroll.style.height = '36vw'
+  } else {
+    historywrap.style.top = '350px'
+    historyScroll.style.height = '19vw'
+  }
+})
+
+// dragBtn.addEventListener(
+//   'mousedown',
+//   function (e) {
+//     e.preventDefault()
+
+//     // //최대값과 최소값
+//     // if (e.clientY < 90) {
+//     //   return
+//     // }
+//     // if (e.clientY > 350) {
+//     //   return
+//     // }
+
+//     // initX = object.offsetLeft
+//     initY = historywrap.offsetTop
+//     // firstX = e.pageX
+//     firstY = e.pageY
+
+//     historywrap.addEventListener('mousemove', dragIt, false)
+
+//     window.addEventListener(
+//       'mouseup',
+//       function () {
+//         historywrap.removeEventListener('mousemove', dragIt, false)
+//       },
+//       false
+//     )
+//   },
+//   false
+// )
+
+// object.addEventListener(
+//   'touchstart',
+//   function (e) {
+//     e.preventDefault()
+//     initX = object.offsetLeft
+//     initY = object.offsetTop
+//     var touch = e.touches
+//     firstX = touch[0].pageX
+//     firstY = touch[0].pageY
+
+//     object.addEventListener('touchmove', swipeIt, false)
+
+//     window.addEventListener(
+//       'touchend',
+//       function (e) {
+//         e.preventDefault()
+//         object.removeEventListener('touchmove', swipeIt, false)
+//       },
+//       false
+//     )
+//   },
+//   false
+// )
+
+// function dragIt(e) {
+//   initYEl.textContent = initY
+//   firstYEl.textContent = firstY
+//   // this.style.left = initX + e.pageX - firstX + 'px'
+//   historywrap.style.top = initY + e.pageY - firstY + 'px'
+//   console.log(historywrap.style.top)
+// }
+
+// // function swipeIt(e) {
+// //   var contact = e.touches
+// //   // this.style.left = initX + contact[0].pageX - firstX + 'px'
+// //   historywrap.style.top = initY + contact[0].pageY - firstY + 'px'
+// // }
+
+// function checkboundary() {
+//   // let outer = slider.getBoundingClientRect()
+//   // let inner = innerSlider.getBoundingClientRect()
+
+//   if (parseInt(historywrap.offsetTop) > 250) {
+//     historywrap.style.top = '350px'
+//   } else if (parseInt(historywrap.offsetTop) < 200) {
+//     historywrap.style.top = '90px'
+//   }
+// }
